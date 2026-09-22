f98232368c4d4d110ac1c740fc8e489824d675da
snagurum <58689575+snagurum@users.noreply.github.com;snagurum@rediffmail.com>
2026-09-22 11:17:33 +0530

    Using CustomAuthenticationProvider

---
+++ src/main/java/com/security/authserver/config/CustomAuthenticationProvider.java
@@ -0,0 +1,43 @@
+package com.security.authserver.config;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Component
+@Slf4j 
+@RequiredArgsConstructor
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        log.info("CustomAuthenticationProvider");
+        if (passwordEncoder.matches(pwd, userDetails.getPassword())) {
+            // Fetch Age details and perform validation to check if age >18
+            return new UsernamePasswordAuthenticationToken(username,pwd,userDetails.getAuthorities());
+        }else {
+            throw new BadCredentialsException("Invalid password!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
